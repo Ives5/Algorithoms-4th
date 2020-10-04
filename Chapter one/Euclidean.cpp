3470448bf5872eecf3c19580c96b2501d6d4b9fa
Ives5 <1145479175@qq.com>
2020-10-04 16:55:42 +0800

    Chpater one

---
+++ Chapter one/Euclidean.cpp
@@ -0,0 +1,19 @@
+/*
+欧几里得算法：计算两个非负整数p和q的最大公约数：
+若q是0，则最大公约数为p.否则，将p除以q得到余数r，
+p和q的最大公约数即为q和r的最大公约数
+*/
+#include<iostream>
+using namespace std;
+int gcd(int,int);
+int gcd(int p,int q)
+{
+	if (q == 0)return p;
+	int r = p % q;
+	return gcd(q, r);
+}
+int main()
+{
+	cout << gcd(2, 6);
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Hello world！
+Change the java codes in Algorithms 4th into c++ codes.
+
+Start from 2020/10/4
+
+Contributor : Fu Yuhan
